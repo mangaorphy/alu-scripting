796f794a552d9b358a79a6097826962516cb3893
mangaorphy <o.manga@alustudent.com>
2024-07-02 16:29:19 +0200

    api_advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,15 +4,26 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    Retrieve the number of subscribers for a given subreddit.
+
+    Parameters:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit. Returns 0 if the subreddit does not exist or an error occurs.
+
+    """
     # Set the custom User-Agent
-    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
-                             "(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}
+    header = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}
 
     # Construct the URL for the subreddit API
     url = f"https://api.reddit.com/r/{subreddit}/about"
 
     # Send a GET request to the API with the custom User-Agent
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=header, allow_redirects=False)
 
     # Check if the request was successful
     if response.status_code == 200:
--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,14 +1,27 @@
 import requests
 
+
 def top_ten(subreddit):
+    """
+    Retrieve and print the titles of the top 10 posts in a given subreddit.
+
+    Parameters:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+
+    """
     # Set the custom User-Agent
-    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}
+    header = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}
 
     # Construct the URL for the subreddit API
     url = f"https://api.reddit.com/r/{subreddit}/hot"
 
     # Send a GET request to the API with the custom User-Agent
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=header, allow_redirects=False)
 
     # Check if the request was successful
     if response.status_code == 200:
@@ -20,7 +33,7 @@ def top_ten(subreddit):
         for i in range(10):
             if i < len(posts):
                 post_title = posts[i]["data"]["title"]
-                print(f"{i+1}. {post_title}")
+                print(f"{post_title}")
             else:
                 break
     else:
