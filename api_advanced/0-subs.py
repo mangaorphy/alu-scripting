a979ef48f17aa10a7fc8871da3922820e7e1130e
mangaorphy <o.manga@alustudent.com>
2024-07-17 02:01:56 -0700

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """
-Contains the number_of_subscribers function
+function that queries the 'Reddit API' and returns the number of subscribers
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or not isinstance(subreddit, str):
+    """
+    number of subscribers
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "MyBot/1.0"}  # avoid Too Many Requests error
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()["data"]
+        return data["subscribers"]
+    else:
         return 0
-    r = requests.get(
-        'http://www.reddit.com/r/{}/about.json'.format(subreddit),
-        headers={
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
-                          "AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/126.0.0.0 Safari/537.36"
-        }).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
